74485fb2ce8a121816ca9d0335426ddaa15ae1a2
dsl1888888@163.com <dsl1888888@163.com>
2022-04-21 01:14:57 +0800

    add

--- 08-k8s-start-aliyun.sh
+++ 08-k8s-start-aliyun.sh
@@ -6,3 +6,4 @@ kubectl apply -f /opt/k8s-spring-cloud-direct-run-aliyun/docker-k8s-eureka/k8s-e
 kubectl apply -f /opt/k8s-spring-cloud-direct-run-aliyun/docker-k8s-app01/k8s-app01.yaml
 kubectl apply -f /opt/k8s-spring-cloud-direct-run-aliyun/docker-k8s-app02/k8s-app02.yaml
 kubectl apply -f /opt/k8s-spring-cloud-direct-run-aliyun/docker-k8s-zuul/k8s-api-gateway.yaml
+
